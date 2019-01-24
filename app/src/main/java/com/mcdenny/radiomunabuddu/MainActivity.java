a0f4dd1f5e179cd3c18e0f1ae7f5d72925340b10
Oluka Denis <olukadeno@gmail.com>
2019-01-24 10:00:53 +0300

    * Changed the UI * Added the navigation drawer and pics

--- app/src/main/java/com/mcdenny/radiomunabuddu/MainActivity.java
+++ app/src/main/java/com/mcdenny/radiomunabuddu/MainActivity.java
@@ -2,69 +2,95 @@ package com.mcdenny.radiomunabuddu;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView mPlay, mStop, mPause;
-    private static String URL = "http://markswist.com/markpersonal/B.O.B.-The_Adventures_Of_Bobby_Ray/04-b.o.b.-airplanes_(ft._hayley_williams_of_paramore).mp3";
+    FloatingActionButton mPlay, mStop;
+    SeekBar progressBar;
+    //http://mp3hdfm32.hala.jo:8132
     MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Playing Radio...");
 
-        mPlay = (ImageView) findViewById(R.id.play);
-        mStop = (ImageView) findViewById(R.id.stop);
-        mPause = (ImageView) findViewById(R.id.pause);
+        initializeViews();
+        initializeMediaPlayer();
+    }
+
+    private void initializeViews() {
+        mPlay = (FloatingActionButton) findViewById(R.id.play);
+        mStop = (FloatingActionButton) findViewById(R.id.stop);
+        progressBar = (SeekBar) findViewById(R.id.radio_progressbar);
+        progressBar.setMax(100);
+
+        mPlay.setOnClickListener(v -> playRadio());
+        mStop.setOnClickListener(v -> stopRadio());
+    }
+
+    private void initializeMediaPlayer() {
 
         mediaPlayer = new MediaPlayer(); //init the media player
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //getting audio from internet
+        try {
+            String URL = "http://markswist.com/markpersonal/B.O.B.-The_Adventures_Of_Bobby_Ray/04-b.o.b.-airplanes_(ft._hayley_williams_of_paramore).mp3";
+            //String URL = "http://mp3hdfm32.hala.jo:8132";
+            mediaPlayer.setDataSource(URL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //playing audio
-        mPlay.setOnClickListener(new View.OnClickListener() {
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
             @Override
-            public void onClick(View v) {
-                //getting audio from internet
-                try {
-                    mediaPlayer.setDataSource(URL);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                //buffer the audio
-                try{
-                    mediaPlayer.prepare();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                //start playing the audio file
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                progressBar.setSecondaryProgress(percent);
+                Log.i("Buffering", "" + percent);
+            }
+        });
+
+    }
+
+    private void playRadio() {
+        try {
+            mediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
                 mediaPlayer.start();
-                Toast.makeText(MainActivity.this, "Song playing...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Radio playing...", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        //pausing the audio
-        mPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.pause();
-                Toast.makeText(MainActivity.this, "Song Paused..", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void stopRadio() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            initializeMediaPlayer();
+            progressBar.resetPivot();
+            Toast.makeText(MainActivity.this, "Radio stopped..", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-
-        //stopping the audio
-        mStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.stop();
-                Toast.makeText(MainActivity.this, "Song stopped..", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
     }
 }
